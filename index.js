a38a572ca792f8714195813b81a57257f10cae1d
Hibryda <barmesser@gmail.com>
2016-10-24 09:33:45 +0200

    replaced dependencies with js only ones to make it work with NW.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const SHA3 = require('keccakjs')
-const secp256k1 = require('secp256k1')
+const SHA3 = require('keccakjs-browseronly')
+const secp256k1 = require('secp256k1/js')
 const assert = require('assert')
 const rlp = require('rlp')
 const BN = require('bn.js')
